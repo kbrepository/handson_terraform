b78a92f622db20f305550dabcb086ed66389f33c
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2022-09-18 14:27:40 +0530

    Defined initial web security group

--- prod.tf
+++ prod.tf
@@ -7,3 +7,26 @@ resource "aws_s3_bucket" "tf_bucket" {
   bucket = "tf-bucket-kb"
   acl = "private"
 }
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard HTTP and HTTPS ports inbound and everything outbound"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+}
