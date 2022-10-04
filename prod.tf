b07b549b96c5ee0dd0cfe8ad8898476c7bd2ba74
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2022-10-04 14:40:59 +0530

    WEb instance and Elastic IP

--- prod.tf
+++ prod.tf
@@ -28,5 +28,37 @@ resource "aws_security_group" "prod_web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terrafrom" : "true"
+  }
+}
+
+resource "aws_instance" "prod_web" {
+  ami           = "ami-033af604bb2b4e9b5"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terrafrom" : "true"
+  }
+  
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terrafrom" : "true"
+  }
   
 }
