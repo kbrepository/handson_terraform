58c27b6935dd6b49e73a82022de81c5310597b9e
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2024-01-30 18:26:24 +0530

    Added variable to modules.

---
+++ main/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = var.region
+  profile = "awskb"
+}
+
+module "vpc"{
+  source = "../vpc"
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+}
+
+module "ec2_instance" {
+  source              = "../ec2-instance"
+  ami_id              = var.ami_id
+  instance_type       = var.instance_type
+  instance_name       = var.instance_name
+  security_group_name = var.security_group_name
+}
