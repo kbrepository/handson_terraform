ff6a5ec81dc01efe17b2050a817e97030dbc3400
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2024-01-30 10:19:17 +0530

    Added VPC Network to the configuration.

--- main.tf
+++ main.tf
@@ -3,6 +3,50 @@ provider "aws" {
   profile = "awskb"
 }
 
+# VPC Creation
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnets
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+}
+
+# Route Table association
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+
 resource "aws_instance" "example" {
   ami           = "ami-0a3c3a20c09d6f377"
   instance_type = "t2.micro"
